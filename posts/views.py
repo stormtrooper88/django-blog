2b4a75fe84f2b6b0730f27a01f140b4dd757b8fa
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-22 09:47:38 +0000

    Added create your base templates and css styles but issues with the base html and the post page

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .models import Post
 from .forms import BlogPostForm
 
 
